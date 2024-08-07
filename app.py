d157d8eb2d3f758548ea87e457399aef3abeffe3
w1k5 <vmichalska00@gmail.com>
2024-08-06 21:37:32 -0400

    Revert "segregate code into ui and scraper"
    This reverts commit 79ccfc6a7832e98d306a9b2fa3ad79eb2fd37f3a.

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pandas as pd
 from shapely.geometry import Point
 from flask_frozen import Freezer
 
-if not os.path.exists('./static'):
-    os.makedirs('./static')
+if not os.path.exists('static'):
+    os.makedirs('static')
 
 app = Flask(__name__)
 freezer = Freezer(app)
--- tax-bill-scrapper.py
+++ tax-bill-scrapper.py
@@ -7,6 +7,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
